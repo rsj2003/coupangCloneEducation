ecdb44cb2191d9b3bf0856f5121a02d17b85ff56
rsj2003 <rsj2003@naver.com>
2020-12-24 09:03:52 +0900

    이 작은 커밋이 큰 변화가 되기를

---
+++ src/main/java/com/seungjae/CoupangCloneApplication.java
@@ -0,0 +1,13 @@
+package com.seungjae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoupangCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoupangCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seungjae/demo/CuopangCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seungjae.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuopangCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
